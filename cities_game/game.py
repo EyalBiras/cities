2f4500381626d626d49de02e08e10ac40b2eaa9f
EyalBiras <eyalbiras@gmail.com>
2024-12-09 19:48:38 +0200

    Improved readability

--- cities_game/game.py
+++ cities_game/game.py
@@ -5,28 +5,33 @@ from cities_game.player import Player
 
 
 class Game:
-    def __init__(self, player: Player, enemy: Player, neutral: Player) -> None:
-        self.player = player
-        self.enemy = enemy
-        self.neutral = neutral
+    def __init__(self, player: Player, enemy: Player, neutral: Player, turn: int) -> None:
+        self.__player = player
+        self.__enemy = enemy
+        self.__neutral = neutral
+        self.__turn = turn
 
     def get_enemy_cities(self) -> list[City]:
-        return self.enemy.cities
+        return self.__enemy.cities
 
     def get_enemy_city_capital(self) -> Capital:
-        return self.enemy.capital_city
+        return self.__enemy.capital_city
 
     def get_enemy_groups(self) -> list[Group]:
-        return self.enemy.groups
+        return self.__enemy.groups
 
     def get_my_cities(self) -> list[City]:
-        return self.player.cities
+        return self.__player.cities
 
     def get_my_city_capital(self) -> Capital:
-        return self.player.capital_city
+        return self.__player.capital_city
 
     def get_my_groups(self) -> list[Group]:
-        return self.player.groups
+        return self.__player.groups
 
     def get_neutral_cities(self) -> list[City]:
-        return self.neutral.cities
+        return self.__neutral.cities
+
+    @property
+    def turn(self):
+        return self.__turn
